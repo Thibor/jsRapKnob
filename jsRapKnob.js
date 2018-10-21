686317c01d215c57330661cd0df91d1717c16594
Thibor <thibor@vp.pl>
2018-10-22 00:32:34 +0100

    Add files via upload

--- jsRapKnob.js
+++ jsRapKnob.js
@@ -5,13 +5,14 @@ return this.each(function(){
 	this.opt = $.extend({
 		enabled:true,
 		position:0,
+		steps:0,
 		caption:'',
 		onMouseUp:null,
 		onChange:null
 	},options);
+	$(this).addClass('rapKnob');
 	let base = this;
 	let button = 0;
-	$(this).addClass('rapKnob');
 	let inBar = $('<div>').addClass('rapKnobIn').appendTo($(this));
 	let caption = $('<div>').addClass('rapKnobCaption').text(this.opt.caption).appendTo($(this));
 	let w = $(this).width();
@@ -38,10 +39,10 @@ return this.each(function(){
 			mouseup:function(e){
 				button = 0;
 				if(this.opt.onMouseUp)
-					this.opt.onMouseUp.call(this,this.opt.position);
+					this.opt.onMouseUp.call(this,this.opt.position,this.opt.step);
 			}
 		});	
-		
+	
 	function Update(e){
 		let cx = e.clientX - $(base)[0].getBoundingClientRect().left;
 		let cy = e.clientY - $(base)[0].getBoundingClientRect().top;
@@ -54,11 +55,18 @@ return this.each(function(){
 	}
 			
 	this.SetPosition = function(p){
+		let s = 0;
+		if(this.opt.steps > 1){
+			let fs = 1 / (this.opt.steps - 1);
+			s = Math.floor(p / fs + .5);
+			p = s * fs;
+		}
 		this.opt.position = p;
+		this.opt.step = s;
 		let d = p * 270 - 135;
-		inBar.css('transform','translate(-50%,-50%) rotate(' + d + 'deg) translate(0,-50%)');
+		inBar.css('transform','translate(-50%,-50%) rotate(' + d + 'deg) translate(0,-200%)');
 		if(this.opt.onChange)
-			this.opt.onChange.call(this,p);
+			this.opt.onChange.call(this,p,s);
 	}
 	
 	this.SetPosition(this.opt.position);
